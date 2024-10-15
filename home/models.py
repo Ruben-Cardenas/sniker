6568bc3160e9f6bf72b4c671f56f1477fee85855
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-10-14 18:49:39 -0600

    first commit

---
+++ home/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/')
+
+    def __str__(self):
+        return self.nombre
+
+class Carrito(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(default=1)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.cantidad} x {self.producto.nombre}'
