081381938116f7dcb8b83a23cce59d5b4a041c18
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-10-16 18:36:12 -0600

    #finalizado

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from .views import homeview
 from .views import carritoview, pagoview
 
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path('',homeview, name='home'),
-    path('carritoview', carritoview, name='carritoview'), 
-    path('pago/', pagoview, name='home') 
+    path('', views.homeview, name='homeview'),  # Esta es la vista principal 'home' (el "index")
+    path('carrito/', views.carritoview, name='carritoview'),  # Vista para el carrito
+    path('pago/', views.pagoview, name='pagoview'),  # Vista para el pago
 ]
 
 
